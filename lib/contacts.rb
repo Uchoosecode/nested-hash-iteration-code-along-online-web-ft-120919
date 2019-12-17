f2d80d902f55c74cb60dbf641f2cf73aecc077bc
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-17 14:29:00 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,3 +1,4 @@
+require_relative '../lib/contacts.rb'
 require 'pry'
 
   # This is the array we will be passing into the remove_strawberry method
@@ -15,5 +16,15 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+    
+      if person == "Freddy Mercury"
+        contact_details_hash.each do |attribute, data|
+          
+          if person == " Freddy Mercury"
+            contact_details_hash.each do |attribute, data|
+    binding.pry
+          end
+    end
+  end
 end
